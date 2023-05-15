975c62cc89b3075134b5ef7111259e8d3278bd04
ferdi <dinaldese@gmail.com>
2023-05-15 19:06:16 +0700

    API

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const router = express.Router()
-const db = require('./../models')
-const User = db.User
-const authenticateTokenMiddleware = require('../middleware/authentication')
+const express = require("express");
+const router = express.Router();
+const db = require("./../models");
+const User = db.User;
+const authenticateTokenMiddleware = require("../middleware/authentication");
 
 // Inject middleware as global middleware
-router.use(authenticateTokenMiddleware)
+router.use(authenticateTokenMiddleware);
 
 // GET /users
-router.get('/api/users', async (request, response) => {
-  const users = await User.findAll({ offset: request.query.page, limit: request.query.size })
-  const userCount = await User.count()
+router.get("/api/users", async (request, response) => {
+  const users = await User.findAll({ offset: request.query.page, limit: request.query.size });
+  const userCount = await User.count();
 
   return response.status(200).json({
     data: users,
@@ -18,48 +18,48 @@ router.get('/api/users', async (request, response) => {
       page: request.query.page,
       count: userCount,
       size: users.length,
-    }
-  })
-})
+    },
+  });
+});
 
 // GET /users/:id
-router.get('/api/users/:id', async (request, response) => {
-  const user = await User.findByPk(request.params.id)
+router.get("/api/users/:id", async (request, response) => {
+  const user = await User.findByPk(request.params.id);
 
-  if(!user) return response.status(404).json({message: 'Users not found'})
+  if (!user) return response.status(404).json({ message: "Users not found" });
 
-  return response.status(200).json({data: user})
-})
+  return response.status(200).json({ data: user });
+});
 
 // POST /users
-router.post('/api/users', async (request, response) => {
-  const user = await User.create(request.body)
+router.post("/api/users", async (request, response) => {
+  const user = await User.create(request.body);
 
-  if(!user) return response.status(422).json({message: 'Failed create user. Please try again'})
+  if (!user) return response.status(422).json({ message: "Failed create user. Please try again" });
 
-  return response.status(200).json({data: user})
-})
+  return response.status(200).json({ data: user });
+});
 
 // PUT /users/:id
-router.put('/api/users/:id', async (request, response) => {
-  const user = await User.findByPk(request.params.id)
+router.put("/api/users/:id", async (request, response) => {
+  const user = await User.findByPk(request.params.id);
 
-  if(!user) return response.status(404).json({message: 'Users not found'})
+  if (!user) return response.status(404).json({ message: "Users not found" });
 
-  User.update(request.body, { where: {id: request.params.id }})
+  User.update(request.body, { where: { id: request.params.id } });
 
-  return response.status(200).json({ message: 'User updated'})
-})
+  return response.status(200).json({ message: "User updated" });
+});
 
 // DELETE /users/:id
-router.delete('/api/users/:id', async (request, response) => {
-  const user = await User.findByPk(request.params.id)
+router.delete("/api/users/:id", async (request, response) => {
+  const user = await User.findByPk(request.params.id);
 
-  if(!user) return response.status(404).json({message: 'Users not found'})
+  if (!user) return response.status(404).json({ message: "Users not found" });
 
-  User.destroy({where: { id: request.params.id }})
+  User.destroy({ where: { id: request.params.id } });
 
-  return response.status(200).json({data: user})
-})
+  return response.status(200).json({ data: user });
+});
 
-module.exports = router
+module.exports = router;
